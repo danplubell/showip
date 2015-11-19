924c15a4338ee7a43911755ad9efc64456842978
Dan Plubell <dplubell@cerner.com>
2015-11-19 14:12:40 -0600

    add usage, clean up

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import           Network.Socket
+import           System.Environment
+
+myHints::AddrInfo
+myHints = defaultHints {addrSocketType =  Stream}
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  case args of
+    []  -> putStrLn "Usage: showIP hostname"
+    (h:_)     ->   do
+                    addrInfoList <- getAddrInfo (Just myHints) (Just h)  Nothing
+                    mapM_  showit addrInfoList
+                    where showit a  = putStrLn $ show (addrFamily a) ++ ":" ++ show  (addrAddress a)
+
